719effac8df2cd4c8845a6b68bab44c230d1cb17
ddelamare <ddelam93@gmail.com>
2024-09-14 23:34:21 +0000

    Moved other token areas

--- src/Board/Board.tsx
+++ src/Board/Board.tsx
@@ -29,36 +29,24 @@ class Board extends Component<BoardProps> {
           <HexGrid width="100vw" height="100vh" viewBox="-50 -50 100 100">
             <Layout size={{ x: 6, y: 6 }}>
               {this.props.G.map.map((hex) => <Hex key={hex.id} onClick={() => this.props.moves.placeToken(hex)} data={hex}>1</Hex>)}
-              <svg width="70" height="10" x="-48" y="-50" viewBox='0 0 70 10' style={{fontSize: "3px"}}>
-                <rect width="100%" height="100%"  rx="1" fillOpacity="0" strokeOpacity="1" stroke='black' strokeWidth="1"></rect>
+              <svg width="35" height="6" x="-49" y="-50" viewBox='0 0 70 10' style={{fontSize: "3px"}}>
+                <rect width="100%" height="100%"  rx="1" fillOpacity="0" strokeOpacity="1" stroke='black' strokeWidth=".5"></rect>
                 {this.props.G.actionPool.map((token, i) => {
-                    return (<g transform={`translate(${10 * (i + .5) },5)`} key={this.props.playerID + "ap" + token.id} onClick={() => { this.props.moves.pickAction(i); }}><Token  type={token.type} owner={null} rank={null} renderSvgTag={false}></Token></g>)
+                    return (<g transform={`translate(${11 * (i + .5) },5)`} key={this.props.playerID + "ap" + token.id} onClick={() => { this.props.moves.pickAction(i); }}><Token  type={token.type} owner={null} rank={null} renderSvgTag={false}></Token></g>)
                   })}
               </svg>
+              {KeysAsArray(this.props.G.players).map((player, pid) => {
+              return <svg width="35" height="6" x="-49" y={-50 + ((pid + 1) * 6)} viewBox='0 0 70 10' style={{fontSize: "3px"}}>
+                <rect width="100%" height="100%"  rx="1" fillOpacity="0" strokeOpacity="1" stroke='black' strokeWidth=".5"></rect>
+                {player.availableActions.map((token, i) => {
+                  return (<g transform={`translate(${11 * (i + .5) },5)`} key={this.props.playerID + "ava" + token.id} className={token.id === player.selectedToken ? "glow" : "shadow"} onClick={() => { this.props.moves.selectToken(token.id) }}><Token type={token.type} owner={token.owner} rank={null} renderSvgTag={false}></Token></g>)
+                })}
+              </svg>
+            })}
+
             </Layout>
           </HexGrid>
         </div>
-        <table>
-          <tbody>
-            <tr>
-
-              <td></td>
-            </tr>
-          </tbody>
-        </table>
-        <table>
-          <tbody>
-            {KeysAsArray(this.props.G.players).map((player, pid) => {
-              return <tr key={"avatr" + pid}>
-                {player.availableActions.map((token, i) => {
-                  return (<td key={this.props.playerID + "ava" + token.id} className={token.id === player.selectedToken ? "glow" : "shadow"} onClick={() => { this.props.moves.selectToken(token.id) }}><Token type={token.type} owner={token.owner} rank={null} renderSvgTag={true}></Token></td>)
-                })}
-              </tr>
-            })}
-
-          </tbody>
-
-        </table>
         <button onClick={() => this.props.events.endPhase?.()}>End Phase</button>
       </div>
     );
